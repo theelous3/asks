7d2a0fcd99b3e5c87f339f96471875e50c686139
raylu <lurayl@gmail.com>
2017-02-17 21:36:02 -0800

    setup.py cleanup
    Make executable
    licence -> license

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python3
+
 from setuptools import setup
 
-
-setup(name='asks',
-      description='asks - async http',
-      long_description='asks is an async http lib',
-      licence='MIT',
-      version='0.0.2',
-      author='Mark Jameson - aka theelous3',
-      url='https://github.com/theelous3/asks',
-      packages=['asks'],
-      install_requires=['curio'],
-      classifiers=['Programming Language :: Python :: 3'])
+setup(
+    name='asks',
+    description='asks - async http',
+    long_description='asks is an async http lib',
+    license='MIT',
+    version='0.0.2',
+    author='Mark Jameson - aka theelous3',
+    url='https://github.com/theelous3/asks',
+    packages=['asks'],
+    install_requires=['curio'],
+    classifiers=['Programming Language :: Python :: 3']
+)
