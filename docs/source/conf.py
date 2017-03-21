bb2d964f3f01789a701b3794790c44d8e1c45cf3
theelous3 <theegrandmaster@gmail.com>
2017-03-21 20:41:59 +0000

    resize doc font

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -112,7 +112,7 @@ latex_elements = {
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-    'pointsize': '10pt',
+    'pointsize': '13pt',
 
     # Additional stuff for the LaTeX preamble.
     #
