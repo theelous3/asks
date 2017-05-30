60725e7038d67bd4da442bcaad13a09848f6ef1f
theelous3 <theegrandmaster@gmail.com>
2017-05-30 03:55:56 +0100

    removed html from docs, += build

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+from sys import version_info
+
+ver = version_info[:2]
+if ver < (3, 6):
+    raise SystemExit('Sorry! asks requires python 3.6.0 or later.')
 
 setup(
     name='asks',
     description='asks - async http',
     long_description='asks is an async http lib for curio',
     license='MIT',
-    version='0.1.1',
+    version='0.2.1',
     author='Mark Jameson - aka theelous3',
     url='https://github.com/theelous3/asks',
     packages=['asks'],
