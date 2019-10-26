c6b9ba9d50f8243e4582d65d6796a30f0dbc3744
millefalcon <hanish0019@gmail.com>
2019-10-26 17:40:25 +0530

    Fixed unknown_kwarg check

--- asks/sessions.py
+++ asks/sessions.py
@@ -171,7 +171,7 @@ class BaseSession(metaclass=ABCMeta):
         unknown_kwarg = next((k for k in kwargs if k not in ALLOWED_KWARGS), sentinel)
         # if `unknown_kwargs` is not `sentinel`, the some unkown kwargs are in `kwargs`
         # and hence we should raise a TypeError
-        if unknown_kwargs is not sentinel:
+        if unknown_kwarg is not sentinel:
             raise TypeError("request() got an unexpected keyword argument{!r}".format(unknown_kwarg)) from None
 
         timeout = kwargs.get('timeout', None)
