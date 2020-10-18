fac6da0c3882bfdb6cebb627dadcd16b6e4a3960
theelous3 <theegrandmaster@gmail.com>
2020-10-18 12:52:30 +0100

    reworked how urls are constructed with the base_location/endpoint method

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='asks - async http',
     long_description='asks is an async http lib for curio, trio and asyncio',
     license='MIT',
-    version='2.4.10',
+    version='2.4.11',
     author='Mark Jameson - aka theelous3',
     url='https://github.com/theelous3/asks',
     packages=['asks'],
--- tests/test_http_utils.py
+++ tests/test_http_utils.py
@@ -3,7 +3,7 @@ import gzip
 
 import pytest
 
-from asks import http_utils
+from asks import http_utils, Session
 
 INPUT_DATA = b"abcdefghijklmnopqrstuvwxyz"
 UNICODE_INPUT_DATA = "\U0001f408\U0001F431" * 5
@@ -43,3 +43,50 @@ def test_decompress_decoding():
         res = decompressor.send(b)
         result += res
     assert result == UNICODE_INPUT_DATA
+
+
+@pytest.mark.parametrize(
+    "url_segments,expected",
+    [
+        (
+            ("http://example.com", "", ""),
+            "http://example.com"
+        ),
+        (
+            ("http://example.com", "some_endpoint", ""),
+            "http://example.com/some_endpoint"
+        ),
+        (
+            ("http://example.com/", "/some_endpoint", ""),
+            "http://example.com/some_endpoint"
+        ),
+        (
+            ("http://example.com/", "/some_endpoint", "some/path"),
+            "http://example.com/some_endpoint/some/path"
+        ),
+        (
+            ("http://example.com/", "/some_endpoint", "/some/path/"),
+            "http://example.com/some_endpoint/some/path"
+        ),
+        (
+            ("http://example.com/", "/some_endpoint/", "/some/path/"),
+            "http://example.com/some_endpoint/some/path"
+        ),
+        (
+            ("http://example.com/", "", "/some/path/"),
+            "http://example.com/some/path"
+        )
+    ]
+)
+def test_api_url_construction(url_segments, expected):
+    base_location, endpoint, path = url_segments
+    session = Session(base_location=base_location, endpoint=endpoint)
+    constructed_url = session._make_url(path)
+    assert constructed_url == expected
+
+
+def test_api_url_construction_but_no_base():
+    base_location, endpoint, path = ("", "/some_endpoint", "/some_path")
+    session = Session(base_location=base_location, endpoint=endpoint)
+    with pytest.raises(ValueError):
+        constructed_url = session._make_url(path)
